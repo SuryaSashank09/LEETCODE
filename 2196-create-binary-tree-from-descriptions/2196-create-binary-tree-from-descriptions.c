ed17f1fd013c9a05b162508fec0545309218aceb
SuryaSashank09 <basinasuryasashank@gmail.com>
2026-06-07 13:58:59 +0530

    Time: 52 ms (38.46%), Space: 142.5 MB (7.69%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.c
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.c
@@ -2,16 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Provided by LeetCode:
-// struct TreeNode {
-//     int val;
-//     struct TreeNode *left;
-//     struct TreeNode *right;
-// };
 
 struct TreeNode* createBinaryTree(int** descriptions, int descriptionsSize, int* descriptionsColSize) {
-    // Map from value -> TreeNode*
-    // Since values are <= 1e5, we can use an array instead of a hash map
     struct TreeNode** nodes = (struct TreeNode**)calloc(100001, sizeof(struct TreeNode*));
     int* hasParent = (int*)calloc(100001, sizeof(int));
 
@@ -20,14 +12,13 @@ struct TreeNode* createBinaryTree(int** descriptions, int descriptionsSize, int*
         int child = descriptions[i][1];
         int isLeft = descriptions[i][2];
 
-        // Create parent if not exists
         if (!nodes[parent]) {
             nodes[parent] = (struct TreeNode*)malloc(sizeof(struct TreeNode));
             nodes[parent]->val = parent;
             nodes[parent]->left = NULL;
             nodes[parent]->right = NULL;
         }
-        // Create child if not exists
+ 
         if (!nodes[child]) {
             nodes[child] = (struct TreeNode*)malloc(sizeof(struct TreeNode));
             nodes[child]->val = child;
@@ -42,11 +33,11 @@ struct TreeNode* createBinaryTree(int** descriptions, int descriptionsSize, int*
             nodes[parent]->right = nodes[child];
         }
 
-        // Mark child as having a parent
+
         hasParent[child] = 1;
     }
 
-    // Find root (the one that never appears as a child)
+   
     struct TreeNode* root = NULL;
     for (int i = 0; i < descriptionsSize; i++) {
         int parent = descriptions[i][0];
