201fa262640ecd504cb9634a46651ee352933c03
SuryaSashank09 <basinasuryasashank@gmail.com>
2026-06-08 11:10:59 +0530

    Time: 8 ms (33.67%), Space: 99 MB (76.53%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+int* pivotArray(int* nums, int numsSize, int pivot, int* returnSize) {
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    *returnSize = numsSize;
+
+    int idx = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] < pivot) {
+            result[idx++] = nums[i];
+        }
+    }
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == pivot) {
+            result[idx++] = nums[i];
+        }
+    }
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] > pivot) {
+            result[idx++] = nums[i];
+        }
+    }
+
+    return result;
+}
